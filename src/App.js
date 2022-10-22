839de66ec17a72f6f1af62ec11cc6705b2dc428b
godsfavourkio <kiofavour@gmail.com>
2022-10-22 16:08:39 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import { InputForm } from './components/form_component/input-form';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='firstDiv'>
+        <div className='up'>
+          <h3>Lottery Display</h3>
+        </div>
+        <br></br>
+        <br></br>
+        <div className='middle'>
+          <h2 className='font2'>A few clicks away<br></br>from creating your<br></br>Lottery Display</h2>
+        </div>
+        <div className='down'>
+        </div>
+      </div>
+      <div className='secondDiv'>
+        <h1>Register</h1>
+        <br></br>
+        <h2>Manage all your lottery efficiently</h2>
+        <p>Let's get you all set up o you can verify you r perosnal account and login</p>
+
+        <div className='upperFormDiv'>
+          <InputForm title="First Name"/>
+          <InputForm title="Last Name"/>
+        </div>
+
+        <div className='middleFormDiv'>
+          <InputForm title="Phone Number"/>
+          <InputForm title="Email"/>
+        </div>
+
+        <div className='lowerFormDiv'>
+          <InputForm title="Password"/>
+          <InputForm title="Confirm Password"/>
+        </div>
+      </div>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/components/form_component/input-form.js
@@ -0,0 +1,10 @@
+import "./input-form.css"
+
+export const InputForm = (props) => (
+    <div>
+        <div>
+            <h2 className='pp'>{props.title}</h2>
+            <input className="customForm"></input>
+        </div>
+    </div>
+)
